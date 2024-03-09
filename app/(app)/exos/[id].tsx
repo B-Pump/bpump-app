12bf7071cdabab336e49a4f5cb49d44b65d716da
wiizz <wiizz76.contact@gmail.com>
2024-03-09 19:01:24 +0100

    fix: exos video resizemode

--- app/(app)/exos/[id].tsx
+++ app/(app)/exos/[id].tsx
@@ -42,7 +42,7 @@ export default function Exos() {
             alignSelf: "center",
             width: "100%",
             height: 200,
-            borderRadius: 30,
+            borderRadius: 15,
         },
     });
 
@@ -106,7 +106,7 @@ export default function Exos() {
                                 }}
                                 style={styles.video}
                                 useNativeControls
-                                resizeMode={ResizeMode.CONTAIN}
+                                resizeMode={ResizeMode.COVER}
                                 onPlaybackStatusUpdate={(status) => setVideo(() => status)}
                             />
                         </View>
