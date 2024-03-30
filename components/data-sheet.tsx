0e9d268cd3d318056476a267c2c7dc970e40e4f2
wiizz <wiizz76.contact@gmail.com>
2024-03-30 13:46:56 +0100

    fix: bottom sheet backdrop

--- components/data-sheet.tsx
+++ components/data-sheet.tsx
@@ -1,29 +1,37 @@
-import { BottomSheetModal } from "@gorhom/bottom-sheet";
-import React, { forwardRef, useMemo } from "react";
+import { BottomSheetBackdrop, BottomSheetModal } from "@gorhom/bottom-sheet";
+import { ReactNode, forwardRef, useCallback, useMemo } from "react";
 import { Text, View } from "react-native";
 
+import { useColorScheme } from "@/lib/color";
+
 type Ref = BottomSheetModal;
+
 interface SheetProps {
     title: string;
     data: string[];
-    children: React.ReactNode;
+    children: ReactNode;
 }
 
 export const Sheet = forwardRef<Ref, SheetProps>((props, ref) => {
-    const snapPoints = useMemo(() => ["75%"], []);
+    const { isDarkColorScheme } = useColorScheme();
+
+    const snapPoints = useMemo(() => ["25%", "75%"], []);
+    const renderBackdrop = useCallback(
+        (props: any) => <BottomSheetBackdrop opacity={0.15} appearsOnIndex={0} disappearsOnIndex={-1} {...props} />,
+        [],
+    );
 
     return (
         <BottomSheetModal
             ref={ref}
             snapPoints={snapPoints}
             enablePanDownToClose
-            backgroundStyle={{
-                backgroundColor: "white",
-            }}
+            backdropComponent={renderBackdrop}
+            backgroundStyle={{ backgroundColor: isDarkColorScheme ? "#363636" : "white" }}
         >
             <View className="p-4">
                 <View className="mb-4 items-center">
-                    <Text className="text-xl font-bold">{props.title}</Text>
+                    <Text className="text-xl font-bold text-foreground">{props.title}</Text>
                 </View>
                 {props.children}
             </View>
